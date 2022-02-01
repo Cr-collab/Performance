c95d5766b482c6a38a198c813a9fc1eb26b0fc8f
Cristiano Azevdo <cristiccorrea@gmail.com>
2022-02-01 19:05:08 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,16 @@
+module.exports = () => {
+  const data = {
+    products = []
+  }
+  
+ for (let i = 0; i < 1000; i++) {
+    data.products.push({
+      id : i + 1,
+      name : `Product${i + 1} `,
+      price: `${i + 1 + i}0,0${i+1}`
+    })
+ }
+
+ return data
+
+}
