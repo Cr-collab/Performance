5e5988315402bf7ecb354750604bb9092b22684e
Cristiano Azevdo <cristiccorrea@gmail.com>
2022-02-03 19:27:59 -0300

    usando o memo

--- components/SearchResults.tsx
+++ components/SearchResults.tsx
@@ -23,3 +23,16 @@ export function SearchResults({results} : SearchResultsProps){
     </div>
   );
 }
+
+
+/**
+ *  A primeira funcionalidade vai ajudar  evitar que agente faça essa recalculação 
+ *  principalmente em componentes filhos  , quando na verdade sabemos que não vai 
+ *  mudar por causa do conteudo pai.
+ * 
+ *  cilco de renderização 
+ *  1 - gera uma nova versão do componente 
+ *  2 - compara a versão anterior a nova 
+ *  3 - Se houverem alterações vai atualizar o que alterou.
+ * 
+ */
